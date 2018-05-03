ccb8d88ef08cad46c2162d6c61c2bf45b93f5678
taogeT <zwt@TGOffice.localdomain>
2018-05-03 16:27:52 +0800

    add new ZOL spider for notebook PC.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # script-collection
 常用脚本工具集合
 
-### 1. 中关村手机爬虫
+### 1. 中关村爬虫
 
 遍历所有的手机名称、价格、参数信息。
 
 ` scrapy crawl cellphone `
+
+遍历所有的笔记本电脑名称、价格、参数信息。
+
+` scrapy crawl notebook `
--- zolSpider/zolSpider/settings.py
+++ zolSpider/zolSpider/settings.py
@@ -23,7 +23,7 @@ USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Ge
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
